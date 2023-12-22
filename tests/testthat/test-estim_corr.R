8fd7b75985ed447aad7b3f14a480c184bbee4e65
eduardklap <et.klapwijk@gmail.com>
2023-12-22 16:56:54 +0100

    add snapshot tests for figures

--- tests/testthat/test-estim_corr.R
+++ tests/testthat/test-estim_corr.R
@@ -8,3 +8,17 @@ test_that("output class is correct", {
   expect_s3_class(expected$tbl_total, "data.frame")
 })
 
+# set seed for creating reproducible plots for plot tests
+set.seed(1234)
+
+test_that("check that plots look as expected", {
+  expected_fig <- estim_corr(gambling,
+                             c("lnacc_self_winvsloss", "age"),
+                             20:30, 11)
+  vdiffr::expect_doppelganger(
+    title = "create fig corr",
+    fig = expected_fig$fig_corr,)
+  vdiffr::expect_doppelganger(
+    title = "create fig corr nozero",
+    fig = expected_fig$fig_corr_nozero,)
+})
--- tests/testthat/test-estim_diff.R
+++ tests/testthat/test-estim_diff.R
@@ -9,3 +9,21 @@ test_that("output class is correct", {
   expect_s3_class(expected$tbl_total, "data.frame")
 })
 
+# set seed for creating reproducible plots for plot tests
+set.seed(1234)
+
+test_that("check that plots look as expected", {
+  expected_fig <- estim_diff(feedback,
+                         c("mfg_learning", "mfg_application"),
+                         20:30, 11)
+    vdiffr::expect_doppelganger(
+      title = "create fig diff",
+      fig = expected_fig$fig_diff,)
+    vdiffr::expect_doppelganger(
+      title = "create fig nozero",
+      fig = expected_fig$fig_nozero,)
+    vdiffr::expect_doppelganger(
+      title = "create fig d nozero",
+      fig = expected_fig$fig_d_nozero,)
+})
+
